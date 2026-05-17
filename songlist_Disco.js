ef0da1fb9238c82b11a277d304be7e0671ca574e
qianyey204-lab <qianyey204@gmail.com>
2026-05-17 16:55:41 +0900

    Add files via upload

---
+++ songlist_Disco.js
@@ -0,0 +1,27 @@
+// songlist_Disco.js
+export const songListDisco = [
+  {
+    no: 1,
+    title: "Boogie Wonderland",
+    artist: "Eath-Wind&Fire",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/Disco/Boogie%20Wonderland_Eath-Wind&Fire.mp3"
+  },
+  {
+    no: 2,
+    title: "Night Fever",
+    artist: "Eath-Wind&Fire",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/Disco/Night%20Fever_Bee%20Gees&Fire.mp3"
+  },
+  {
+    no: 3,
+    title: "September",
+    artist: "Eath-Wind&Fire",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/Disco/September_Eath-Wind&Fire.mp3"
+  },
+  {
+    no: 4,
+    title: "Stayin' Alive",
+    artist: "Bee Gees",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/Disco/Stayin'%20Alive_Bee%20Gees.mp3"
+  }
+];
